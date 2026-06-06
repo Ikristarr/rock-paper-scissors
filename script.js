a10a90c6030f5c286bd50af278798008b19ab0a1
ikristar <iykejchurchill@gmail.com>
2026-06-06 19:17:49 -0400

    Add computer choice logic,Add human choice logic, Add game round logic, Build rock paper scissors game

--- script.js
+++ script.js
@@ -17,5 +17,45 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
+function playGame() {
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "paper" && computerChoice === "rock" ||
+    humanChoice === "scissors" && computerChoice === "paper"
+  ) {
+    humanScore++;
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+  } else {
+    computerScore++;
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+
+  console.log("Human score: " + humanScore);
+  console.log("Computer score: " + computerScore);
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+
+if (humanScore > computerScore) {
+  console.log("Final result: You won the game!");
+} else if (computerScore > humanScore) {
+  console.log("Final result: You lost the game!");
+} else {
+  console.log("Final result: The game is a tie!");
+}
+
+}
+
+playGame();
